73b99258b8dc1c047ab3aa478db8f5db1b15259d
Tandin Dorji <65913825+tandynhd@users.noreply.github.com>
2022-10-07 16:24:09 +0700

    updates

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
 from flask import Flask, render_template, request
-import pickle
 import numpy as np
-
-model = pickle.load(open('iri.pkl', 'rb'))
+import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+from tqdm import tqdm
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["get"])
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path)
+                       for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+
+def extract_VGG16(tensor):
+    from keras.applications.vgg16 import VGG16, preprocess_input
+    return VGG16(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+
+def extract_InceptionV3(tensor):
+    from keras.applications.inception_v3 import InceptionV3, preprocess_input
+    return InceptionV3(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+
+@app.route('/', methods=["GET"])
 def man():
     return render_template('home.html')
-
-
-# @app.route('/predict', methods=['POST'])
-# def home():
-#     data1 = request.form['a']
-#     data2 = request.form['b']
-#     data3 = request.form['c']
-#     data4 = request.form['d']
-#     arr = np.array([[data1, data2, data3, data4]])
-#     pred = model.predict(arr)
-#     return render_template('after.html', data=pred)
 
 
 @app.route('/', methods=['POST'])
@@ -28,7 +43,25 @@ def home():
     image = request.files['imagefile']
     image_path = "./static/images/dog.jpg"
     image.save(image_path)
-    return render_template('after.html')
+
+    model = tf.keras.models.load_model("./model/model1.h5")
+
+    dog_names = ['Affenpinscher', 'Afghan_hound', 'Airedale_terrier', 'Akita', 'Alaskan_malamute', 'American_eskimo_dog', 'American_foxhound', 'American_staffordshire_terrier', 'American_water_spaniel', 'Anatolian_shepherd_dog', 'Australian_cattle_dog', 'Australian_shepherd', 'Australian_terrier', 'Basenji', 'Basset_hound', 'Beagle', 'Bearded_collie', 'Beauceron', 'Bedlington_terrier', 'Belgian_malinois', 'Belgian_sheepdog', 'Belgian_tervuren', 'Bernese_mountain_dog', 'Bichon_frise', 'Black_and_tan_coonhound', 'Black_russian_terrier', 'Bloodhound', 'Bluetick_coonhound', 'Border_collie', 'Border_terrier', 'Borzoi', 'Boston_terrier', 'Bouvier_des_flandres', 'Boxer', 'Boykin_spaniel', 'Briard', 'Brittany', 'Brussels_griffon', 'Bull_terrier', 'Bulldog', 'Bullmastiff', 'Cairn_terrier', 'Canaan_dog', 'Cane_corso', 'Cardigan_welsh_corgi', 'Cavalier_king_charles_spaniel', 'Chesapeake_bay_retriever', 'Chihuahua', 'Chinese_crested', 'Chinese_shar-pei', 'Chow_chow', 'Clumber_spaniel', 'Cocker_spaniel', 'Collie', 'Curly-coated_retriever', 'Dachshund', 'Dalmatian', 'Dandie_dinmont_terrier', 'Doberman_pinscher', 'Dogue_de_bordeaux', 'English_cocker_spaniel', 'English_setter', 'English_springer_spaniel', 'English_toy_spaniel', 'Entlebucher_mountain_dog', 'Field_spaniel', 'Finnish_spitz',
+                 'Flat-coated_retriever', 'French_bulldog', 'German_pinscher', 'German_shepherd_dog', 'German_shorthaired_pointer', 'German_wirehaired_pointer', 'Giant_schnauzer', 'Glen_of_imaal_terrier', 'Golden_retriever', 'Gordon_setter', 'Great_dane', 'Great_pyrenees', 'Greater_swiss_mountain_dog', 'Greyhound', 'Havanese', 'Ibizan_hound', 'Icelandic_sheepdog', 'Irish_red_and_white_setter', 'Irish_setter', 'Irish_terrier', 'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound', 'Japanese_chin', 'Keeshond', 'Kerry_blue_terrier', 'Komondor', 'Kuvasz', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberger', 'Lhasa_apso', 'Lowchen', 'Maltese', 'Manchester_terrier', 'Mastiff', 'Miniature_schnauzer', 'Neapolitan_mastiff', 'Newfoundland', 'Norfolk_terrier', 'Norwegian_buhund', 'Norwegian_elkhound', 'Norwegian_lundehund', 'Norwich_terrier', 'Nova_scotia_duck_tolling_retriever', 'Old_english_sheepdog', 'Otterhound', 'Papillon', 'Parson_russell_terrier', 'Pekingese', 'Pembroke_welsh_corgi', 'Petit_basset_griffon_vendeen', 'Pharaoh_hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese_water_dog', 'Saint_bernard', 'Silky_terrier', 'Smooth_fox_terrier', 'Tibetan_mastiff', 'Welsh_springer_spaniel', 'Wirehaired_pointing_griffon', 'Xoloitzcuintli', 'Yorkshire_terrier']
+
+    img_path = './static/images/dog.jpg'
+
+    bottleneck_feature1 = extract_InceptionV3(path_to_tensor(img_path))
+    bottleneck_feature2 = extract_VGG16(path_to_tensor(img_path))
+
+    print(bottleneck_feature1.shape)
+    print(bottleneck_feature2.shape)
+
+    pred = model.predict([bottleneck_feature1, bottleneck_feature2])
+
+    print(dog_names[np.argmax(pred)])
+
+    return render_template('after.html', data=dog_names[np.argmax(pred)])
 
 
 if __name__ == "__main__":
